d31aa0b8b1527d7b51a73eed17423ebd5de2e6d3
playanon <cleberdsmds@gmail.com>
2018-09-29 17:56:11 -0300

    config

--- topicosA/config/config.js
+++ topicosA/config/config.js
@@ -1,3 +1,11 @@
 exports.config = {
     "chave": "topicosCC2015"
 }
+
+exports.tipo_usuario = {
+    "admin" : 1,
+    "coordenador" : 2,
+    "discente" : 3,
+    "doscente" : 4,
+    "secretaria" : 5
+}
