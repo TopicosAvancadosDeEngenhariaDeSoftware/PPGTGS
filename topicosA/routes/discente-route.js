6a77056f72c33f80aa76074abbd43ad64a8dd7fd
Mahuan Abdala <mahuanabdala@hotmail.com>
2018-10-03 19:38:32 -0300

    ajustando merge

--- topicosA/routes/discente-route.js
+++ topicosA/routes/discente-route.js
@@ -5,20 +5,6 @@ var controllerDiscente = require('../controller/discente-controller');
 /* GET home page. */
 router.get('/registro', controllerDiscente.cadastrarDiscente);
 
-// CRUD = Create, Read, Update, Delete
-// Create = POST method
-// READ   = GET method
-// UPDATE = PUT method
-// DELETE = DELETE method
-
-const router = require('express').Router();
-
-router.post('/discentes',discente_controller.cadastrarDiscente);
-router.put('/discentes/:id_discente', discente_controller.editarDiscente);
-router.delete('/discentes/:id_discente', discente_controller.excluirDiscente);
-router.get('/discentes/:id_discente', discente_controller.recuperarDiscenteId);
-//router.get('discentes/nome', discente_controller.recuperarDiscenteNome);
-//router.get('/discentes', discente_controller.recuperarDiscentes);
 
 
 module.exports = router;
