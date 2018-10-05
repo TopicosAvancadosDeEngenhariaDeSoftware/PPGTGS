caeea1fb8ec8eae4d3b6f44b41f3249ae75f892e
playanon <cleberdsmds@gmail.com>
2018-10-05 18:16:32 -0300

    alteração cadastro discente

--- topicosA/controller/discente-controller.js
+++ topicosA/controller/discente-controller.js
@@ -2,6 +2,7 @@
 
 const paisesDao = require('../dao/pais-dao');
 const titulosDao = require('../dao/titulo-dao');
+const docentesDao = require('../dao/docente-dao');
 
 exports.cadastrarDiscente = async (req, res, next) => {
         var pDao = new paisesDao(req.connection);
@@ -10,10 +11,11 @@ exports.cadastrarDiscente = async (req, res, next) => {
                 var tDao = new titulosDao(req.connection);
                 tDao.recuperarTitulos((err, resultados_titulos) => {
                         if(err) return next(err);
-                        res.render('discentes-registro', {paises : resultado_paises, titulos : resultados_titulos});
-                })
-                 
-        });
-
-        
+                        var dDao = new docentesDao(req.connection);
+                        dDao.recuperarDocentesSomenteNome((err, resultados_doscentes) =>{
+                                if(err) return next(err);
+                                res.render('discentes-registro', {paises : resultado_paises, titulos : resultados_titulos, doscentes: resultados_doscentes});
+                        });
+                });      
+        });   
 }
---
+++ topicosA/dao/docente-dao.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');  
+
+module.exports = class DoscenteDao{
+    constructor(connection){
+        this._connection = connection;
+    }
+
+    recuperarDocentesSomenteNome(callback){
+        var sql = "SELECT id_docente, nome FROM Docente;"
+        var params = [];
+        sql = mysql.format(sql, params);
+        this._connection.query(sql, (error, results) =>{
+            if(error){
+                callback(error, null);
+            }else{
+                callback(error,results);
+            }
+        });
+    }
+}
