dab83fc9568fdaf7500152bde294623ae899a983
Mahuan Abdala <mahuanabdala@hotmail.com>
2018-11-04 20:01:42 -0200

    alteração indicadores discente, requisições com id_situacao

--- topicosA/routes/json/discente-route.js
+++ topicosA/routes/json/discente-route.js
@@ -16,14 +16,15 @@ router.delete('/discentes/:id_discente', discente_controller.excluirDiscente);
 router.get('/discentes/:id_discente', discente_controller.recuperarDiscenteId);
 
 //indicadores
-router.get('/discentes/instituicoes/total', discente_controller.RecuperarDiscenteInstituicao);
-router.get('/discentes/cargos/total', discente_controller.RecuperarDiscenteCargoInstituicao);
-router.get('/discentes/paises/total', discente_controller.RecuperarNacionalidadeDiscente);
-router.get('/discentes/tipos_instituicoes_discente/total', discente_controller.RecuperarTipoInstituicaoDiscente);
-router.get('/discentes/titulos/total', discente_controller.RecuperarTituloDiscente);
-router.get('/discentes/tipos_discente/total',discente_controller.RecuperarTipoDiscente);
-router.get('/discentes/tipos_discente/:id_tipo_discente',discente_controller.RecuperarDiscentePorTipoDiscente);
+router.get('/discentes/situacoes_discente/:id_situacao/instituicoes/total', discente_controller.RecuperarDiscenteInstituicao);
+router.get('/discentes/situacoes_discente/:id_situacao/cargos/total', discente_controller.RecuperarDiscenteCargoInstituicao);
+router.get('/discentes/situacoes_discente/:id_situacao/paises/total', discente_controller.RecuperarNacionalidadeDiscente);
+router.get('/discentes/situacoes_discente/:id_situacao/tipos_instituicoes_discente/total', discente_controller.RecuperarTipoInstituicaoDiscente);
+router.get('/discentes/situacoes_discente/:id_situacao/titulos/total', discente_controller.RecuperarTituloDiscente);
+router.get('/discentes/situacoes_discente/:id_situacao/tipos_discente/total',discente_controller.RecuperarTipoDiscente);
+router.get('/discentes/situacoes_discente/:id_situacao/tipos_discente/:id_tipo_discente',discente_controller.RecuperarDiscentePorTipoDiscente);
 router.get('/discentes/situacoes_discente/:id_situacao',discente_controller.RecuperarDiscentePorSituacao);
+router.get('/discentes/situacoes_discente/quantidade/total', discente_controller.RecuperarQuantidadeDiscentePorSituacao);
 
 //router.get('/discentes', discente_controller.recuperarDiscentes);
 
