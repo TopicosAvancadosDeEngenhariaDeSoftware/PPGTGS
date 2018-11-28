2c95361de5c7075e40308491b9f5d15dad9880ea
Mahuan Abdala <mahuanabdala@hotmail.com>
2018-11-28 13:30:54 -0200

    route discente add excluir

--- topicosA/routes/json/discente-route.js
+++ topicosA/routes/json/discente-route.js
@@ -12,7 +12,8 @@ const router = require('express').Router();
 
 router.post('/discentes',discente_controller.cadastrarDiscente);
 router.put('/discentes/:id_discente', discente_controller.editarDiscente);
-router.delete('/discentes/:id_discente', discente_controller.excluirDiscente);
+router.delete('/discentes/:id_discente', discente_controller.excluirDiscenteLogico);
+router.delete('/discentes/excluir/:id_discente', discente_controller.excluirDiscente);
 router.get('/discentes/:id_discente', discente_controller.recuperarDiscenteId);
 
 //indicadores
